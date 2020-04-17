bac211904ae6c18bab97651e308fbdf0c8dcf211
Stalyon <pierre.stainton@gmail.com>
2020-04-17 15:37:12 +0200

    Nouvelle implémentation pour l'alarme sonore

--- src/main/java/com/stalyon/ogame/utils/AlertService.java
+++ src/main/java/com/stalyon/ogame/utils/AlertService.java
@@ -1,19 +1,28 @@
 package com.stalyon.ogame.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import sun.audio.AudioPlayer;
-import sun.audio.AudioStream;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
-import java.io.InputStream;
 
 @Service
 public class AlertService {
 
-    public void runAlarm() throws IOException {
-        InputStream inputStream = new ClassPathResource("alarm.wav").getInputStream();
-        AudioStream audioStream = new AudioStream(inputStream);
-        AudioPlayer.player.start(audioStream);
+    private static Logger LOGGER = LoggerFactory.getLogger(AlertService.class);
+
+    public void runAlarm() {
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(new ClassPathResource("alarm.wav").getInputStream()));
+            clip.start();
+        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 }
--- src/main/java/com/stalyon/ogame/utils/MessageService.java
+++ src/main/java/com/stalyon/ogame/utils/MessageService.java
@@ -6,8 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Service
 public class MessageService {
 
@@ -29,11 +27,7 @@ public class MessageService {
         LOGGER.info(message);
 
         if (withAlert) {
-            try {
-                this.alertService.runAlarm();
-            } catch (IOException e) {
-                LOGGER.error(e.getMessage(), e);
-            }
+            this.alertService.runAlarm();
         }
 
         if (withMail) {
